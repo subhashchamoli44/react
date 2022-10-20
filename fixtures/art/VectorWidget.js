bdba967654626b1894d6a959d2b902ab26422679
subhash chamoli <v-sucham@microsoft.com>
2022-10-20 13:32:55 +0530

    Update VectorWidget.js

--- fixtures/art/VectorWidget.js
+++ fixtures/art/VectorWidget.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-'use strict';
+'use strict';ddd
 
 var Circle = require('react-art/Circle');
 var React = require('react');
